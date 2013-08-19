c4ac5d0513369acc5eaa0e686fcd2798002d06e6
Andreas Eriksson <andreas@codered.se>
2013-08-19 22:12:22 +0200

    frontend

--- config/application.rb
+++ config/application.rb
@@ -37,8 +37,8 @@ module Btfoto
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     
-    config.assets.precompile += %w( admin/theme.css admin/backend.css backend.js frontend.js frontend.css.scss )
-    
+    config.assets.precompile += %w( frontend.css admin/theme.css admin/backend.css )
+    #backend.js frontend.js frontend.css.scss
   end
 end
 
