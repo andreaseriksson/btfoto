9e96173b1924ca8a4a508943d81cc977e07125c8
Andreas Eriksson <andreas@codered.se>
2013-08-27 19:59:16 +0200

    Added better data

--- spec/factories/order_items.rb
+++ spec/factories/order_items.rb
@@ -2,12 +2,12 @@
 
 FactoryGirl.define do
   factory :order_item do
-    order nil
-    product nil
+    order
+    product
     quantity 1
     price "9.99"
     vat "9.99"
     discount "9.99"
-    image_nr "MyString"
+    image_nr "12Torn-F1-5544-22a"
   end
 end
