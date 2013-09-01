1d1ffd900efdb6eb404c387a80bb0e63c5979b97
Andreas Eriksson <andreas@codered.se>
2013-09-01 13:08:44 +0200

    Added user-validation against whitelist

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+  validates :email, presence: true, uniqueness: true, format: { 
+    with: /\A([\A@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i 
+  }
+  validate :allowed_to_register, on: :create
+  
+  private
+  
+  def allowed_to_register
+    allowed_emails = CONFIG[:allowed_emails].split(" ")
+    errors.add(:email, 'Not allowed') unless allowed_emails.include?(email)
+  end
+  
 end
