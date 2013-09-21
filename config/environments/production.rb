11d79887d6866bb07f42a1e9e851b8bd279f6f7e
Andreas Eriksson <andreas@codered.se>
2013-09-21 09:21:31 +0200

    Fixed settings for sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ Btfoto::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   
-  config.action_mailer.default_url_options = { :host => "www.btfoto.se" }
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.default_url_options = { host: "www.btfoto.se", from: "info@btfoto.se" }
   
 end
