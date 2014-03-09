e0870321e15c346df5dd8517debce02fd1061283
Andreas Eriksson <andreas@codered.se>
2014-03-09 22:24:55 +0100

    Extracted newsitem to helper

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,19 +1,26 @@
 module PagesHelper
   
   def page_markup(content)
-    
     content_tag :section, class: 'mainContentWrap' do
       content_tag :div, class: 'container mainContent' do
         content_tag :div, class: 'row-fluid' do
           content_tag :div, class: 'span12' do
             content = content_tag(:div, class: 'row-fluid featuredContent') { content }
             content << content_tag(:div, class: 'divider dotted clearfix') { content_tag(:span) }
-            content.html_safe
+            content
           end
         end
       end
     end
-
   end
   
+  def news_item(news)
+    content_tag(:p) do   
+      text = content_tag(:strong) { news.published_at.strftime("%Y-%m-%d") }
+      text << link_to(news.title, news_path(news))
+      text << "<br>".html_safe
+      text << link_to(news.preamble, news_path(news))
+      text.html_safe
+    end
+  end
 end
