0d01bc54f1cbc4c2b6ba9fbb9102456c0304e43f
Andreas Eriksson <andreas@codered.se>
2014-09-21 21:59:19 +0200

    Updated config

--- config/application.rb
+++ config/application.rb
@@ -37,8 +37,7 @@ module Btfoto
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.i18n.default_locale = :sv
-    config.i18n.enforce_available_locales = false
-    I18n.config.enforce_available_locales = false
+    I18n.config.enforce_available_locales = true
     
     config.assets.precompile << %w( frontend.css frontend.js admin/theme.css admin/backend.css backend.js favicon.ico jquery.sidr.min.js jquery.sidr.dark.css jquery.jgrowl.js jquery.jgrowl.css )
   end
