2214ea184d19b12be341ade14507e5b82c7b9229
Andreas Eriksson <andreas@codered.se>
2016-06-07 08:42:44 +0200

    Remove unused files

--- config/application.rb
+++ config/application.rb
@@ -44,7 +44,3 @@ module Btfoto
                                     jquery.jgrowl.css )
   end
 end
-
-CONFIG = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
-CONFIG.merge! CONFIG.fetch(Rails.env, {})
-CONFIG.symbolize_keys!
