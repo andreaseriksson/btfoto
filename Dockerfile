1a03ffae2e4788b9c3ebb532a5b5292313af6f64
Andreas Eriksson <andreas@codered.se>
2016-06-07 21:46:12 +0200

    Update to use Amazon S3 when storing files

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,9 @@ RUN echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" > /etc/
     DEBIAN_FRONTEND=noninteractive \
     apt-get install -y --force-yes libpq-dev
 
+RUN curl -o wkhtmltopdf-0.9.9-static-i386.tar.bz2 https://dl.dropboxusercontent.com/u/10197373/wkhtmltopdf-0.9.9-static-i386.tar.bz2
+RUN tar -vxjf /usr/local/bin/wkhtmltopdf-0.9.9-static-i386.tar.bz2
+RUN mv wkhtmltopdf-i386 /usr/local/bin/wkhtmltopdf
 
 ADD Gemfile /app/Gemfile
 ADD Gemfile.lock /app/Gemfile.lock
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -11,8 +11,8 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include Sprockets::Helpers::IsolatedHelper
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -20,12 +20,9 @@ class PictureUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{model.folder}"
   end
 
-  
-
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
      %w(jpg jpeg)
   end
-  
 end
