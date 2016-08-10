2e6384cf1be4179451323f06a0a4a4080592b1f3
Andreas Eriksson <andreas@codered.se>
2016-08-10 08:38:47 +0200

    HTML-ize the order confirmation email

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,11 +1,9 @@
 class OrderMailer < ActionMailer::Base
 
   add_template_helper(ApplicationHelper)
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.order_mailer.order_confirmation.subject
-  #
+
+  layout 'base_mailer'
+
   def order_confirmation(order)
     @order = order
     payment_type = @order.payment_type == 'cash' ? 'forskottsbetalning' : 'kortbetalning'
