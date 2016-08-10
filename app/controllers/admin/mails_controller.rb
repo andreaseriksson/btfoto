3d6c286fdec0bdb8ca86530383912e3bea3c84ce
Andreas Eriksson <andreas@codered.se>
2016-08-10 17:56:00 +0200

    Complete the mail preview in admin and html emails

---
+++ app/controllers/admin/mails_controller.rb
@@ -0,0 +1,45 @@
+module Admin
+  class MailsController < ApplicationController
+    MAILERS = [ImportMailer, OrderMailer]
+
+    before_action :find_and_build_mail, only: [:show, :preview]
+
+    def index
+      @grouped_emails = grouped_emails
+    end
+
+    def show
+    end
+
+    def preview
+      render text: @mail.body
+    end
+
+    private
+
+    def find_and_build_mail
+      mailer_name, action_name = params[:id].split('|')
+      mailer = MAILERS.find { |mailer| mailer.name == mailer_name }
+      action = mailer.action_methods.find { |action| action.to_s == action_name }
+      mail_params = get_mail_params [mailer_name, action_name]
+      @mail = mailer.send(action, *mail_params)
+    end
+
+    def grouped_emails
+      MAILERS.each_with_object({}) do |mailer, hsh|
+        hsh[mailer.to_s] = mailer.action_methods.map(&:to_s)
+      end
+    end
+
+    def get_mail_params(mail)
+      case mail
+      when %w(ImportMailer notification)
+        order = Order.last
+        [order&.email, order&.order_items&.first&.image_nr]
+      when %w(OrderMailer order_confirmation)
+        order = Order.last
+        [order]
+      end
+    end
+  end
+end
