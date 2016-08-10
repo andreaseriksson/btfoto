a01128de27fc2ba1ed403d664c20abfc870c4281
Andreas Eriksson <andreas@codered.se>
2016-08-10 18:09:49 +0200

    Remove default order

--- app/models/news.rb
+++ app/models/news.rb
@@ -4,6 +4,5 @@ class News < ActiveRecord::Base
 
   validates :title, presence: true
 
-  default_scope -> { order(created_at: :desc) }
   scope :published, -> { where('published_at <= ?', Date.today).order(published_at: :desc) }
 end
