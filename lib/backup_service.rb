93e53376e387d676d12e8db9941619755e47ede1
Andreas Eriksson <andreas@codered.se>
2016-08-28 15:08:41 +0200

    Move from a worker to a normal lib

--- lib/backup_service.rb
+++ lib/backup_service.rb
@@ -1,4 +1,4 @@
-class BackupWorker
+class BackupService
   def self.perform
     env_variables = %w(BT_FOTO_DATABASE_PASSWORD AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_REGION).map do |env_variable|
       "#{env_variable}=#{ENV[env_variable]}"
