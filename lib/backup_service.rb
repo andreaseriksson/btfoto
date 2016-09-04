238ffe2217d0047bf53b5acb28e2a474c2cd4f83
Andreas Eriksson <andreas@codered.se>
2016-09-04 13:33:45 +0200

    Update script and pass in password

--- lib/backup_service.rb
+++ lib/backup_service.rb
@@ -6,11 +6,11 @@ class BackupService
     folder = Time.now.strftime('%Y.%m.%d.%M.%S')
     file_name = 'backup.sql'
 
-    system("pg_dump --format=tar --no-privileges btfoto_#{Rails.env} > #{root_path}/#{file_name}")
+    system("PGPASSWORD=#{ENV['BT_FOTO_DATABASE_PASSWORD']} pg_dump --format=tar --no-privileges -h db -U postgres btfoto_#{Rails.env} > #{root_path}/#{file_name}")
 
     s3 = Aws::S3::Resource.new(region: ENV['AWS_REGION'])
     obj = s3.bucket('codered-dbbackups').object("btfoto/#{folder}/#{file_name}")
-    obj.upload_file('backup.sql')
+    obj.upload_file(file_name)
 
     File.delete(Rails.root.join(file_name))
   end
